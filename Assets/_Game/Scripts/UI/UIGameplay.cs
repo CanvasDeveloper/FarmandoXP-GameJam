e3414e58e603d38973253fa65064b1bca57a5dae
ErickTorres <erickcanvas@gmail.com>
2023-04-01 16:43:41 -0300

    Updated ui

--- Assets/_Game/Scripts/UI/UIGameplay.cs
+++ Assets/_Game/Scripts/UI/UIGameplay.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIGameplay : MonoBehaviour
 {
-    [SerializeField] private GameObject pausePanel;    
-    [SerializeField] private GameObject optionsPanel;    
+    [SerializeField] private Image playerHealth;
+    [SerializeField] private Image playerMana;
+
+    [SerializeField] private Image[] totens;
+
+    [SerializeField] private GameObject pausePanel;            
     [SerializeField] private GameObject gameoverPanel;
 
     private void OnEnable()
     {
         GameManager.Instance.OnPauseStatusChange += UpdatePauseMenu;
-        GameManager.Instance.OnGameOver += OpenGameoverMenu; 
+        GameManager.Instance.OnGameOver += OpenGameoverMenu;
     }
 
     private void OnDisable()
@@ -35,7 +40,6 @@ public class UIGameplay : MonoBehaviour
     private void DisableAllMenus()
     {
         pausePanel.SetActive(false);
-        optionsPanel.SetActive(false);
         gameoverPanel.SetActive(false);
     }
 }
