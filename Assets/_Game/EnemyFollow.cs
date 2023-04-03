fdf19c62975fb7638265e3bfe3ae73e98f7bbc2f
ErickTorres <erickcanvas@gmail.com>
2023-04-02 21:19:29 -0300

    Update EnemyFollow.cs

--- Assets/_Game/EnemyFollow.cs
+++ Assets/_Game/EnemyFollow.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,19 +6,32 @@ public class EnemyFollow : MonoBehaviour
 {
     private static readonly int DirectionXParam = Animator.StringToHash("directionX");
     private static readonly int DirectionYParam = Animator.StringToHash("directionY");
+    private static readonly int AttackParam = Animator.StringToHash("attack");
 
     public Vector3 currentVelocity;
     public NavMeshAgent agent;
+    
+    public PlayerController player;
+    public HealthSystem playerHealth;
     public Transform target;
+
+    [SerializeField] private float damage = 1;
+    [SerializeField] private float timeToAttack = 1;
 
     [SerializeField]
     private Animator anim;
 
+    private bool hasHitted;
+
     // Start is called before the first frame update
     void Start()
     {
-        var agent = GetComponent<NavMeshAgent>();
-        target = FindObjectOfType<PlayerController>().transform;
+        agent = GetComponent<NavMeshAgent>();
+
+        player = FindObjectOfType<PlayerController>();
+        playerHealth = player.GetComponent<HealthSystem>();
+
+        target = player.transform;
 
         agent.updateRotation = false;
         agent.updateUpAxis = false;
@@ -30,16 +42,40 @@ public class EnemyFollow : MonoBehaviour
     {
         agent.SetDestination(target.position);
         currentVelocity = agent.velocity;
+
         if (currentVelocity == Vector3.zero)
             return;
+
+        if (Vector2.Distance(transform.position, target.position) < agent.stoppingDistance)
+        {
+            //perto da arvore
+            if (!hasHitted)
+            {
+                anim.SetTrigger(AttackParam);
+
+                playerHealth.TakeDamage(transform.position, damage);
+
+                hasHitted = true;
+
+                StartCoroutine(WaitForHit());
+            }
+
+            return;
+        }
+
         UpdatePlayerScale();
         UpdateAnimator();
     }
 
-
     private void UpdatePlayerScale()
     {
         transform.localScale = new Vector3(Mathf.Sign(currentVelocity.x), 1, 1);
+    }
+
+    private IEnumerator WaitForHit()
+    {
+        yield return new WaitForSeconds(timeToAttack);
+        hasHitted = false;
     }
 
     private void UpdateAnimator()
